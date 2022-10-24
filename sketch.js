fbc1c114d0b5b4c6b839452e95ad149aad319381
JStickells <116580598+JStickells@users.noreply.github.com>
2022-10-24 16:42:22 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  background(255,204,240);
+}
+
+function draw() {  
+                 
+                 strokeWeight(2);
+                 stroke (255, 20,2)
+                 fill (255,204, 240)
+                 circle(mouseX, mouseY, mouseX)
+                 
+}
